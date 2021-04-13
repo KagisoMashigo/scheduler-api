4e1ccc1aed55df01736a0ebc806067fbbdb9677e
Masterkakes <kagiso.k.mashigo@gmail.com>
2021-04-13 11:07:52 -0400

    CORS fix

--- src/application.js
+++ src/application.js
@@ -37,6 +37,12 @@ module.exports = function application(
   app.use(helmet());
   app.use(bodyparser.json());
 
+  app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
   app.use("/api", days(db));
   app.use("/api", appointments(db, actions.updateAppointment));
   app.use("/api", interviewers(db));
